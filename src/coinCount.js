4ca3c01d3123a98cc5ef46178f34dcb09052118a
Jonathan Cheng <jcheng461@gmail.com>
2022-07-29 22:42:57 +0900

    coinCount returns an array that specifies coin denomination and number needed. WIP: DRY up code, make code more pure

--- src/coinCount.js
+++ src/coinCount.js
@@ -1,14 +1,23 @@
 export default function coinCount(amount, denomValue, coinTally = []) {
-  console.log("Coin Tally array: ", coinTally);
   if (amount === 0) {
     return coinTally;
   } else {
-    // Let's say my amount initially passed in was 103.
-    const remainder = amount % denomValue; // Here, remainder will be 3 -- 103 % 25.
-    // The number pushed will be 4, since 100 / 25 will be four quarters.
+    const remainder = amount % denomValue;
     const thisCoin = [...coinTally];
-    thisCoin.push((amount - remainder) / denomValue);
-    // Here I'll handle reiterations of the recursive function.
+
+    let coinDenomination = "";
+    if (denomValue === 25) {
+      coinDenomination = "quarters";
+    } else if (denomValue === 10) {
+      coinDenomination = "dimes";
+    } else if (denomValue === 5) {
+      coinDenomination = "nickels";
+    } else {
+      coinDenomination = "pennies";
+    }
+    const numCoins = (amount - remainder) / denomValue;
+    thisCoin.push(`${coinDenomination}: ${numCoins}`);
+
     if (remainder > 9 && remainder < 25) {
       return coinCount(remainder, 10, thisCoin);
     }
@@ -18,30 +27,5 @@ export default function coinCount(amount, denomValue, coinTally = []) {
     if (remainder < 5) {
       return coinCount(remainder, 1, thisCoin);
     }
-    // // Once else statement completes, exit.
-    // console.log("This coin: ", thisCoin);
-    // return thisCoin;
   }
 }
-
-// // SAVE for later
-// export default function coinCount(amount, denomValue, coinTally = []) {
-//   if (amount === 0) {
-//     console.log("Coin Tally array: ", coinTally);
-//     return coinTally;
-//   } else {
-//     // Let's say my amount initially passed in was 103.
-//     console.log("coinCount amount: " + amount);
-//     console.log("denomValue amount: " + denomValue);
-//     const remainder = amount % denomValue; // Here, remainder will be 3 -- 103 % 25.
-//     console.log("remaining amount: " + remainder);
-//     // The number pushed will be 4, since 100 / 25 will be four quarters.
-//     const thisCoin = [...coinTally];
-//     thisCoin.push((amount - remainder) / denomValue);
-//     console.log("thisCoin array: ", thisCoin);
-//     // Here I'll handle a reiteration of the recursive function.
-//     if (remainder > 0 && remainder < 5) {
-//       coinCount(remainder, 1, thisCoin);
-//     }
-//   }
-// }
